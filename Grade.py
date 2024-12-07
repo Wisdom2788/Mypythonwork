fe295d56f1377bea5888fbf6387e51562621a0ba
wisdom uzoma <princewizoma22@gmail.com>
2024-12-07 07:06:55 +0100

    wisdom

---
+++ Grade.py
@@ -0,0 +1,13 @@
+	total score = 0
+	average = 0
+	highest = 0	
+	lowest = 0
+	range = 0
+
+While (socre_number < score):
+	
+		number_input = int(input("Enter ten numbers")
+		score_number +=1
+		sum == score_number
+
+		
---
+++ Range.py
@@ -0,0 +1,3 @@
+for print number in range (1, 3000, 2 ):
+ 	if number %2 == 0:
+		print(number):
